010924c5b7b5ce654bc6e315af861f2ae528e37a
cdoedens <carldoedens@gmail.com>
2026-05-06 17:39:01 +1000

    combine files by month

--- mouse/01.1_combine_files.py
+++ mouse/01.1_combine_files.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 # READ PARQUET TABULAR DATA
 data_path = Path('/scratch/er8/cd3022/xgb_datasets/')
-df = pd.concat(
-    pd.read_parquet(f)
-    for f in data_path.glob('*all_training*')
-)
 
-df[df['month'].isin([2, 6, 10])].to_parquet(data_path / "test_months.parquet")
-df[~df['month'].isin([2, 6, 10])].to_parquet(data_path / "train_months.parquet")
+for month in range(1, 13):
+    df = pd.concat(
+        pd.read_parquet(f)
+        for f in data_path.glob(f'all_training_2025-{month:02d}*')
+    )
+    df.to_parquet(data_path / f"all_training_month_{month:02d}.parquet")
